98dfe0b520e76ea0966f798196986387b6108e7c
Hystee <hysteebyhystee@gmail.com>
2018-09-17 12:00:15 -0400

    add test specs with spyOn attached to window

---
+++ bubblesort.js
@@ -0,0 +1,25 @@
+function bubbleSort(array) {
+  function swap(a, b) {
+    // value at a particular index
+  }
+  // function compare(a, b) {
+  //   console.log('bye');
+  // }
+  if (array.length <= 1) return array;
+  for (let i = 0; i < array.length - 1; i++) {
+    // foo
+    compare(array[i], array[i + 1]);
+  }
+}
+
+// function swap(a, b) {
+//   return 'hi';
+// }
+function compare(a, b) {
+  console.log('bye');
+}
+// bubbleSort.compare = function(a, b) {
+//   console.log('bye');
+// };
+
+bubbleSort([1, 2, 3, 4, 5, 6, 7]);
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,9 +1,21 @@
 describe('Bubble Sort', function() {
-  it('handles an empty array', function() {
-    expect(bubbleSort([])).toEqual([]);
+  beforeAll(function() {
+    //    spyOn(bubbleSort, 'swap').and.callThrough(); // replace existing `bubbleSort['swap']` method
+    spyOn(window, 'compare').and.callThrough(); // replace existing `bubbleSort[`compare`] method
   });
-  it('handles an array with one element', function() {
+
+  it('handles an empty array and swaps 0 times', function() {
+    expect(bubbleSort([])).toEqual([]);
+    bubbleSort([]);
+    //    expect(bubbleSort.swap.calls.count()).toEqual(0);
+    expect(window.compare.calls.count()).toEqual(0);
+  });
+
+  it('handles an array with one element and swaps 0 times', function() {
     expect(bubbleSort([1])).toEqual([1]);
+    bubbleSort([1]);
+    //    expect(bubbleSort.swap.calls.count()).toEqual(0);
+    expect(window.compare.calls.count()).toEqual(0);
   });
 
   it('sorts an array of length greater than 1', function() {
@@ -12,4 +24,28 @@ describe('Bubble Sort', function() {
     expect(bubbleSort([1, 2, 3])).toEqual([1, 2, 3]);
     expect(bubbleSort([3, 1, 2])).toEqual([1, 2, 3]);
   });
+
+  it('sorts an array [1,2,3] and swaps 0 times and compares 2 times', function() {
+    bubbleSort([1, 2, 3]);
+    //    expect(bubbleSort.swap.calls.count()).toEqual(0);
+    expect(window.compare.calls.count()).toEqual(2);
+  });
+
+  it('sorts an array [3,2,1] and swaps 3 times and compares 3 times', function() {
+    bubbleSort([3, 2, 1]);
+    //    expect(bubbleSort.swap.calls.count()).toEqual(3);
+    expect(window.compare.calls.count()).toEqual(3);
+  });
+
+  it('sorts an array [3,1,2] and swaps 2 times and compares 3 times', function() {
+    bubbleSort([3, 1, 2]);
+    //    expect(bubbleSort.swap.calls.count()).toEqual(3);
+    expect(window.compare.calls.count()).toEqual(3);
+  });
+
+  /*  it('sorts an array [4,3,2,1] and swaps # times and compares # times', function() {
+    bubbleSort([4, 3, 2, 1]);
+    expect(bubbleSort.swap.calls.count()).toEqual();
+    expect(bubbleSort.compare.calls.count()).toEqual();
+  });*/
 });
