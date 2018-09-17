8d366b78fb4a7faad0d6da70ee97efb8d7751a15
Magdalena Aichinger <magdalena.aichinger@gmail.com>
2018-09-17 12:52:31 -0400

    compare and swap funcitons added

--- bubblesort.js
+++ bubblesort.js
@@ -10,25 +10,25 @@ function bubbleSort(array) {
     let sorted = false;
     for (let i = 0; i < j - 1; i++) {
       // foo
-      compare(array, i, array[i], array[i + 1]);
+      compare(array, i, array[i], array[i + 1], sorted);
     }
   }
 
   return array;
 }
 
-function compare(array, i, a, b) {
+function compare(array, i, a, b, sorted) {
   if (a > b) {
-    swap(array, i, a, b);
+    swap(array, i, a, b, sorted);
   }
-  console.log("compared!");
+  //console.log("compared!");
 }
 
 function swap(array, i, a, b, sorted) {
   //sorted = true;
   array[i] = b;
   array[i+1] = a;
-  console.log("swapped!");
+  //console.log("swapped!");
 }
 
 console.log(bubbleSort([1, 2, 3, 4, 5, 6, 7]));
